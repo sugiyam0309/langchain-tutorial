e49a64dbb6043bfc47fca14fd7a0a0fcda499402
Jun Sugiyama <jun1jun2jun0309@gmail.com>
2025-08-09 23:58:55 +0900

    feat: Build a Chatbot

---
+++ chatbot/main.py
@@ -0,0 +1,111 @@
+from typing import Annotated, Sequence
+from langchain.chat_models import init_chat_model
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage, trim_messages
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from typing_extensions import Annotated, TypedDict
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    language: str
+    
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+        ),
+        MessagesPlaceholder(variable_name="messages"),
+    ]
+)
+
+# Define a new graph
+workflow = StateGraph(state_schema=State)
+
+trimmer = trim_messages(
+    max_tokens=150,
+    strategy="last",
+    token_counter=model,
+    include_system=True,
+    allow_partial=False,
+    start_on="human",
+)
+
+# Define the function that calls the model
+def call_model(state: State):
+    trimmed_messages = trimmer.invoke(state["messages"])
+    prompt = prompt_template.invoke(
+        {"messages": trimmed_messages, "language": state["language"]}
+    )
+    response = model.invoke(prompt)
+    return {"messages": response}
+
+
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+
+config = {"configurable": {"thread_id": "abc123"}}
+
+messages = [
+    SystemMessage(content="you're a good assistant"),
+    HumanMessage(content="hi! I'm bob"),
+    AIMessage(content="hi!"),
+    HumanMessage(content="I like vanilla ice cream"),
+    AIMessage(content="nice"),
+    HumanMessage(content="whats 2 + 2"),
+    AIMessage(content="4"),
+    HumanMessage(content="thanks"),
+    AIMessage(content="no problem!"),
+    HumanMessage(content="having fun?"),
+    AIMessage(content="yes!"),
+]
+
+query = "What's my name?"
+language = "English"
+
+input_messages = messages + [HumanMessage(query)]
+output = app.invoke({"messages": input_messages, "language": language}, config)
+
+output["messages"][-1].pretty_print()
+
+
+config = {"configurable": {"thread_id": "abc678"}}
+query = "What math problem did I ask?"
+language = "English"
+
+print('messages:', messages)
+
+input_messages = messages + [HumanMessage(query)]
+output = app.invoke(
+    {"messages": input_messages, "language": language},
+    config,
+)
+output["messages"][-1].pretty_print()
+
+
+config = {"configurable": {"thread_id": "abc789"}}
+query = "Hi I'm Todd, please tell me a joke."
+language = "English"
+
+input_messages = [HumanMessage(query)]
+for chunk, metadata in app.stream(
+    {"messages": input_messages, "language": language},
+    config,
+    stream_mode="messages",
+):
+    if isinstance(chunk, AIMessage):  # Filter to just model responses
+        print(chunk.content, end="|")
