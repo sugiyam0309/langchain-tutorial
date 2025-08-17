23028a03d287fe74f2a5964c3f4ab654fcd23a1e
Jun Sugiyama <jun1jun2jun0309@gmail.com>
2025-08-17 20:06:05 +0900

    feat:Summarize Text

---
+++ summarization/main.py
@@ -0,0 +1,127 @@
+from typing import List
+from dotenv import load_dotenv
+from langchain_community.document_loaders import WebBaseLoader
+from langchain.chat_models import init_chat_model
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.llm import LLMChain
+from langchain_core.prompts import ChatPromptTemplate
+from langchain import hub
+from langchain_text_splitters import CharacterTextSplitter
+import operator
+from typing import Annotated, List, Literal, TypedDict
+
+from langchain.chains.combine_documents.reduce import (
+    acollapse_docs,
+    split_list_of_docs,
+)
+from langchain_core.documents import Document
+from langgraph.types import Send
+from langgraph.graph import END, START, StateGraph
+
+load_dotenv()
+
+loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+docs = loader.load()
+
+llm = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+map_prompt = hub.pull("rlm/map-prompt")
+
+reduce_template = """
+The following is a set of summaries:
+{docs}
+Take these and distill it into a final, consolidated summary
+of the main themes.
+"""
+
+reduce_prompt = ChatPromptTemplate([("human", reduce_template)])
+
+text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+    chunk_size=1000, chunk_overlap=0
+)
+split_docs = text_splitter.split_documents(docs)
+print(f"Generated {len(split_docs)} documents.")
+
+token_max = 1000
+
+def length_function(documents: List[Document]) -> int:
+    """Get number of tokens for input contents."""
+    return sum(llm.get_num_tokens(doc.page_content) for doc in documents)
+
+class OverallState(TypedDict):
+    contents: List[str]
+    summaries: Annotated[list, operator.add]
+    collapsed_summaries: List[Document]
+    final_summary: str
+
+class SummaryState(TypedDict):
+    content: str
+
+async def generate_summary(state: SummaryState):
+    prompt = map_prompt.invoke(state["content"])
+    response = await llm.ainvoke(prompt)
+    return {"summaries": [response.content]}
+
+def map_summaries(state: OverallState):
+    return [
+        Send("generate_summary", {"content": content}) for content in state["contents"]
+    ]
+
+def collect_summaries(state: OverallState):
+    return {
+        "collapsed_summaries": [Document(summary) for summary in state["summaries"]]
+    }
+
+
+async def _reduce(input: dict) -> str:
+    prompt = reduce_prompt.invoke(input)
+    response = await llm.ainvoke(prompt)
+    return response.content
+
+async def collapse_summaries(state: OverallState):
+    doc_lists = split_list_of_docs(
+        state["collapsed_summaries"], length_function, token_max
+    )
+    results = []
+    for doc_list in doc_lists:
+        results.append(await acollapse_docs(doc_list, _reduce))
+
+    return {"collapsed_summaries": results}
+
+def should_collapse(
+    state: OverallState,
+) -> Literal["collapse_summaries", "generate_final_summary"]:
+    num_tokens = length_function(state["collapsed_summaries"])
+    if num_tokens > token_max:
+        return "collapse_summaries"
+    else:
+        return "generate_final_summary"
+    
+async def generate_final_summary(state: OverallState):
+    response = await _reduce(state["collapsed_summaries"])
+    return {"final_summary": response}
+
+graph = StateGraph(OverallState)
+graph.add_node("generate_summary", generate_summary)
+graph.add_node("collect_summaries", collect_summaries)
+graph.add_node("collapse_summaries", collapse_summaries)
+graph.add_node("generate_final_summary", generate_final_summary)
+
+graph.add_conditional_edges(START, map_summaries, ["generate_summary"])
+graph.add_edge("generate_summary", "collect_summaries")
+graph.add_conditional_edges("collect_summaries", should_collapse)
+graph.add_conditional_edges("collapse_summaries", should_collapse)
+graph.add_edge("generate_final_summary", END)
+
+app = graph.compile()
+
+import asyncio
+
+async def main():
+    async for step in app.astream(
+        {"contents": [doc.page_content for doc in split_docs]},
+        {"recursion_limit": 10},
+    ):
+        print(list(step.keys()))
+
+asyncio.run(main())
